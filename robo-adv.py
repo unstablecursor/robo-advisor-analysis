df4a8fa5dc243086ce278be92ba69d768747267c
Recep Deniz Aksoy <recepdeniza@gmail.com>
2017-02-15 16:53:07 +0300

    ...

---
+++ robo-adv.py
@@ -0,0 +1,8 @@
+import os.path
+import pandas as pd
+import requests
+from datetime import datetime
+from datetime import timedelta
+import matplotlib
+import matplotlib.pyplot as plt
+import pyfolio as pf
